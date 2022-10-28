8407c6a2b8a7d6bae120250b6cc71a7a54e8962f
AJ School <alissa.j636@nebo.local>
2022-10-28 15:03:38 -0600

    End of day, October 28 2022

--- JS/home.js
+++ JS/home.js
@@ -1,5 +1,3 @@
-
-
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
