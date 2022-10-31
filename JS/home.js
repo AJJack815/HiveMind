53c086d8067d6f8a0d694de82605964eed5ea3c7
Arlo (Alissa) Jackson <103011638+AJJack815@users.noreply.github.com>
2022-10-30 21:21:40 -0600

    Sign up

--- JS/home.js
+++ JS/home.js
@@ -14,3 +14,13 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+var modal = document.getElementById('id01');
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
