76f57c17c2efab4340e14c296404d1bbf0c415cb
AJ School <alissa.j636@nebo.local>
2022-11-03 12:51:38 -0600

    End of class

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const admin = require('firebase-admin');
-const bcrypt = require('bcrypt');
-const path = require('path'); 
 
-let staticPath = path.join(__dirname, "public");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const app = express();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-app.use(express.static(staticPath));
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-app.listen(3000, () => {
-    console.log('listening on port 3000.......')
-})
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
