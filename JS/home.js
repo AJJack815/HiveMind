1db35f87fe1de0587052d2890754dd5493270a9b
AJ School <alissa.j636@nebo.local>
2022-11-07 12:19:47 -0700

    Novemeber 7

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
@@ -26,30 +25,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-=======
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimenstions = item.getBoundingClientRect();
-    let containerWidth = containerDimenstions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
-
-var modal = document.getElementById('id01');
-
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
->>>>>>> c572415983d97123c2adfac5a749e0a60372fbeb
