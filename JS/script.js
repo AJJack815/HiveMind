13cbf54420cdf7eccaec5cdff0d7e1d83584ed62
AJ School <alissa.j636@nebo.local>
2022-11-15 12:54:09 -0700

    End of class. 11/15

--- JS/script.js
+++ JS/script.js
@@ -72,3 +72,31 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+const loader = document.querySelector('.loader');
+
+// select inputs 
+const submitBtn = document.querySelector('.submit-btn');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const number = document.querySelector('#number');
+
+submitBtn.addEventListener('click', () => {
+  if(name.value.length < 3){
+      showAlert('name must be 3 letters long');
+  } else if(!email.value.length){
+      showAlert('enter your email');
+  } else if(password.value.length < 8){
+      showAlert('password should be 8 letters long');
+  } else if(!number.value.length){
+      showAlert('enter your phone number');
+  } else if(!Number(number.value) || number.value.length < 10){
+      showAlert('invalid number, please enter valid one');
+  }
+      // submit form
+  }
+)
+if(name.value.length < 3){
+  showAlert('name must be 3 letters long');
+}
